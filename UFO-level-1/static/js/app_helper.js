55e166ade145ace5396848c111268928de3acfda
leila Zintsem a moute <leilazintsemamoute@gmail.com>
2021-06-06 19:29:46 -0500

    get table references where table will be inserted

--- UFO-level-1/static/js/app_helper.js
+++ UFO-level-1/static/js/app_helper.js
@@ -6,6 +6,58 @@
 const tableData = data;
 
 // get table references where table will be inserted
+var tbody = d3.select("tbody");
+
+// // Console.log the weather data from data.js
+// console.log(data);
+
+// // Step 1: Loop Through `data` and console.log each weather report object
+// data.forEach(function(ufo) {
+//   console.log(ufo);
+// });
+
+// // Step 2:  Use d3 to append one table row `tr` for each UFO  object
+// data.forEach(function(ufo) {
+//             console.log(ufo);
+//             var row = tbody.append("tr");
+// });
+
+// // Step 3:  Use `Object.entries` to grab each value
+// data.forEach(function(ufo) {
+//   console.log(ufo);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufo).forEach(function([key, value]) {
+//     console.log(key, value);
+//   });
+// });
+
+// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+// data.forEach(function(ufo) {
+//   console.log(ufo);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufo).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     // in the weather report object
+//     var cell = row.append("td");
+//   });
+// });
+
+// // Step 5: Use d3 to update each cell's text with
+// // weather report values (weekday, date, high, low)
+// data.forEach(function(ufo) {
+//   console.log(ufo);
+//   var row = tbody.append("tr");
+//   Object.entries(ufo).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     // in the weather report object
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
 
 
 function buildTable(data) {
